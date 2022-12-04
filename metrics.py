d52eeafbd91a52fb656aaa3a9d24f28727c725dd
daydreamore <sven.guenther1992@gmail.com>
2022-12-04 01:37:03 +0100

    Added testing notebook and metrics script

---
+++ metrics.py
@@ -0,0 +1,54 @@
+import Levenshtein 
+
+def rtf(model_time, audio_dur):
+    """
+    Real-Time Factor:
+    - model_time: time taken by the system to process input
+    - audio_dur: duration of input audio
+    """
+    return model_time/audio_dur
+
+def wer(ground_truth, model_out):
+    """
+    Word Error Rate:
+    - input
+        - ground_truth: ground truth [str or list]
+        - model_out: model output [str or list]
+    - vars_created
+        - n_sub: number of substitutions in output text compared to ground truth
+        - n_del: number of deletions performed
+        - n_insert: number of insertions performed
+        - n_corr: number of correct words
+    """
+    if isinstance(ground_truth, str):
+        truth = [truth]
+
+    if isinstance(model_out, str):
+        model_out = [model_out]
+
+    if len(model_out) == 0:
+        raise ValueError("model output is empty")
+
+    if model_out.lower().strip() == ground_truth:
+        return 1.0
+
+    else: #if ground_truth != model output
+        n_sub, n_del, n_insert, n_corr = 0, 0, 0, 0
+
+        for input, output in zip(ground_truth, model_out):
+            output = output.lower().strip() #lower and remove trailing spaces
+            editops = Levenshtein.editops(input, output)
+            n_sub_trial = sum(1 if op[0] == "replace" else 0 for op in editops)
+            n_del_trial = sum(1 if op[0] == "delete" else 0 for op in editops)
+            n_sub += n_sub_trial
+            n_del += n_del_trial
+            n_insert += sum(1 if op[0] == "insert" else 0 for op in editops)
+            n_corr += len(input.split()) - (n_sub_trial + n_del_trial)
+        
+        return float(n_sub + n_del + n_insert)/float(n_sub + n_del + n_corr)
+
+
+
+
+
+    
